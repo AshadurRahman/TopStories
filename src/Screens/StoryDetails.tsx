4e82d8f3cc184f130cf4b19a1adedddec5c57c11
AshadurRahman <rshozib@gmail.com>
2021-09-16 16:14:26 +0200

    removed commented out code

--- src/Screens/StoryDetails.tsx
+++ src/Screens/StoryDetails.tsx
@@ -2,7 +2,6 @@ import React, { FC } from 'react';
 import { Button, Text, TouchableOpacity, View } from 'react-native';
 import { Props } from '../Components/Interfaces';
 import styles from '../Components/Styles';
-import WebViews from './WebView';
 
 const StorryDetails: FC<Props> = (props) => {
   const {navigation} = props;
@@ -18,8 +17,6 @@ const StorryDetails: FC<Props> = (props) => {
         <TouchableOpacity style={styles.floatingButton} onPress={() => navigation.navigate('Home')} >
           <Text style={styles.floatIcon}> {'<'} </Text>
         </TouchableOpacity>
-        {/* <WebViews {...props}/> */}
-        {/* <WebView source={{ uri: props.route.params.Url }} /> */}
       </View>
       
   );
